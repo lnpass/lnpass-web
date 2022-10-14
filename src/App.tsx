a4e5fc99d59117eb4a3861cd6a2e21434bb4d55e
theborakompanioni <theborakompanioni+github@gmail.com>
2022-10-14 13:06:36 +0200

    dev: install prettier

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,26 @@ import {
 } from 'react-router-dom'
 import { Button, Card, Label, TextInput, Tooltip } from 'flowbite-react'
 import { HDKey } from '@scure/bip32'
-import { randomBytes } from '@noble/hashes/utils';
+import { randomBytes } from '@noble/hashes/utils'
 import './App.css'
-import { LnpassId, lnpassIdToSeed, seedToLnpassId, toLnpassIdOrThrow } from './utils/lnpassId';
+import { LnpassId, lnpassIdToSeed, seedToLnpassId, toLnpassIdOrThrow } from './utils/lnpassId'
 
 interface AccountCardProps {
   account: HDKey
 }
 function AccountCard({ account }: AccountCardProps) {
-  return (<Card>
-    <pre>
-      {JSON.stringify(account, null, 2)}
-    </pre>
+  return (
+    <Card>
+      <pre>{JSON.stringify(account, null, 2)}</pre>
 
-    <div className="w-1/4">
-      <Button
-        gradientDuoTone="purpleToBlue"
-        size="xl"
-        onClick={() => (null)}
-      >
-        Login
-      </Button>
-    </div>
-  </Card>)
+      <div className="w-1/4">
+        <Button gradientDuoTone="purpleToBlue" size="xl" onClick={() => null}>
+          Login
+        </Button>
+      </div>
+    </Card>
+  )
 }
-
 
 interface IndexProps {
   lnpassId?: LnpassId
@@ -56,24 +51,20 @@ function Main({ lnpassId, logout }: MainProps) {
 
   const accounts = [hdkey.derive(`m/0/2147483647'/1`)]
 
-  return (<div className="p-2">
+  return (
+    <div className="p-2">
       <Tooltip content="Forget seed and logout!">
-        <Button
-          outline={true}
-          gradientDuoTone="purpleToBlue"
-          size="xl"
-          onClick={() => logout()}
-        >
+        <Button outline={true} gradientDuoTone="purpleToBlue" size="xl" onClick={() => logout()}>
           Logout
         </Button>
       </Tooltip>
-      <div className="text-lg">
-        { lnpassId }
-      </div>
+      <div className="text-lg">{lnpassId}</div>
 
-      {accounts.map((it) => (<div key={it.fingerprint}>
-        <AccountCard account={it} />
-      </div>))}
+      {accounts.map((it) => (
+        <div key={it.fingerprint}>
+          <AccountCard account={it} />
+        </div>
+      ))}
     </div>
   )
 }
@@ -96,7 +87,7 @@ function Login({ onSubmit }: LoginProps) {
   const onSubmitButtonClicked = () => {
     try {
       onSubmit(toLnpassIdOrThrow(lnpassIdInput))
-    } catch(e) {
+    } catch (e) {
       console.error(e)
     }
   }
@@ -105,15 +96,10 @@ function Login({ onSubmit }: LoginProps) {
     <div className="flex h-screen justify-center items-center text-3xl">
       <div className="w-full max-w-7xl">
         <Card>
-          <div className="flex justify-center">
-            Welcome
-          </div>
+          <div className="flex justify-center">Welcome</div>
           <div>
             <div className="hidden">
-              <Label
-                htmlFor="lnpass1"
-                value="lnpass1"
-              />
+              <Label htmlFor="lnpass1" value="lnpass1" />
             </div>
             <div className="flex flex-row gap-2 items-center">
               <div className="flex-1">
@@ -145,12 +131,7 @@ function Login({ onSubmit }: LoginProps) {
           </div>
           <div className="flex justify-center">
             <Tooltip content="Create a new identity pool!">
-              <Button
-                outline={true}
-                gradientDuoTone="purpleToBlue"
-                size="xl"
-                onClick={() => onNewButtonClicked()}
-              >
+              <Button outline={true} gradientDuoTone="purpleToBlue" size="xl" onClick={() => onNewButtonClicked()}>
                 <div className="text-xl">New</div>
               </Button>
             </Tooltip>
@@ -168,17 +149,40 @@ function App() {
     createRoutesFromElements(
       <Route
         id="base"
-        element={<div>
-          <Outlet />
-        </div>}
+        element={
+          <div>
+            <Outlet />
+          </div>
+        }
       >
-        <Route id="home" path="/" index element={<Index lnpassId={lnpassId} logout={() => {
-            setLnpassId(undefined)
-          }} />}
+        <Route
+          id="home"
+          path="/"
+          index
+          element={
+            <Index
+              lnpassId={lnpassId}
+              logout={() => {
+                setLnpassId(undefined)
+              }}
+            />
+          }
         />
-        <Route id="login" path="/login" index element={lnpassId ? <Navigate to="/" replace={true} /> : <Login onSubmit={(lnpassId) => {
-            setLnpassId(lnpassId)
-          }}/>}
+        <Route
+          id="login"
+          path="/login"
+          index
+          element={
+            lnpassId ? (
+              <Navigate to="/" replace={true} />
+            ) : (
+              <Login
+                onSubmit={(lnpassId) => {
+                  setLnpassId(lnpassId)
+                }}
+              />
+            )
+          }
         />
         <Route id="404" path="*" element={<Navigate to="/" replace={true} />} />
       </Route>
@@ -187,6 +191,5 @@ function App() {
 
   return <RouterProvider router={router} />
 }
-
 
 export default App
--- src/utils/lnpassId.ts
+++ src/utils/lnpassId.ts
@@ -1,4 +1,3 @@
-
 import { bech32m } from 'bech32'
 
 const LNPASS_ID_PREFIX = 'lnpass'
@@ -10,19 +9,19 @@ type Seed = Uint8Array
 const LIMIT = LNPASS_ID_PREFIX.length + 7 + 128
 
 export const seedToLnpassId = (seed: Seed): LnpassId => {
-    const words = bech32m.toWords(seed)
-    const encoded = bech32m.encode(LNPASS_ID_PREFIX, words, LIMIT) as LnpassId
-    return encoded
+  const words = bech32m.toWords(seed)
+  const encoded = bech32m.encode(LNPASS_ID_PREFIX, words, LIMIT) as LnpassId
+  return encoded
 }
 
 export const lnpassIdToSeed = (id: LnpassId): Seed => {
-    const decoded = bech32m.decode(id, LIMIT)
-    if (decoded.prefix !== LNPASS_ID_PREFIX) {
-        throw new Error('Cannot decode lnpassId: invalid prefix')
-    }
-    return Uint8Array.from(bech32m.fromWords(decoded.words))
+  const decoded = bech32m.decode(id, LIMIT)
+  if (decoded.prefix !== LNPASS_ID_PREFIX) {
+    throw new Error('Cannot decode lnpassId: invalid prefix')
+  }
+  return Uint8Array.from(bech32m.fromWords(decoded.words))
 }
 
 export const toLnpassIdOrThrow = (idLike: string) => {
-   return seedToLnpassId(lnpassIdToSeed(idLike as LnpassId))
+  return seedToLnpassId(lnpassIdToSeed(idLike as LnpassId))
 }
