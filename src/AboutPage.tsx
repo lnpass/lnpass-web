77ce42be42486aa47acab54d5b841be9ed1cc147
theborakompanioni <theborakompanioni+github@gmail.com>
2025-03-28 22:12:18 +0100

    chore: format

--- src/AboutPage.tsx
+++ src/AboutPage.tsx
@@ -7,7 +7,9 @@ export function AboutPage({ name, version }: AboutPageProps) {
   return (
     <>
       <h2 className="text-3xl font-bold tracking-tighter">About</h2>
-      <div className="text-lg text-slate-500 break-all">{name} {version}</div>
+      <div className="text-lg text-slate-500 break-all">
+        {name} {version}
+      </div>
 
       <div className="mt-4"></div>
     </>
